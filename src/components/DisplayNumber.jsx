eda49150550ea9df7b64add0d1da1bcb9ee926f8
KimYeJin <sophisticateo_o@naver.com>
2023-10-23 16:00:38 +0900

    redux 없는 react 컴포넌트 상태 연결

---
+++ src/components/DisplayNumber.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class DisplayNumber extends Component {
+    render(){
+      return(
+      <div>
+        <h1>Display Number</h1>
+        <input type="text" value={this.props.number} readOnly />
+      </div>
+      );
+    }  
+}
+
+export default DisplayNumber;
+  
